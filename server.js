4fbf2c5d85a8fbb471b58a10fd4af515c553e60b
karlijuhkam <karli.juhkam@gmail.com>
2018-09-18 22:11:39 +0300

    posting to database works | TODO: GET all the news

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost:27017'); // connect to our database
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,7 +28,7 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log('Midagi toimub :O');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -34,19 +37,30 @@ router.get('/', function(req, res) {
     res.json({ message: 'API töötab!' });   
 });
 
-//fetches all the news
-router.route('/news').get(function(req, res) {
-    res.json({ message: 'GET /news töötab!' });  
-});
+
 
 //new post
-router.route('/news').post(function(req, res) {
-    res.json({ message: 'POST /news töötab!' });  
+router.route('/news')
+    .post(function(req, res) {
+        var news = new News();
+        news.title = req.body.title;
+        // save the post and check for errors
+        news.save(function(err) {
+            if (err)
+            res.send(err);
+
+            res.json({ message: 'Post created!' });
+        })
+
+    .get(function(req, res) {
+        News.find(function(err, news) {
+            if (err)
+            res.send(err);
+
+            res.json(news);
+        });
+    });
 });
-
-
-
-
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
